e017b4c55ba4305ee7e40625bd38921fabd5f49b
Justin Chu <justinchuby@users.noreply.github.com>
2024-05-26 19:28:39 -0700

    Format

--- tests/longformer.py
+++ tests/longformer.py
@@ -8,7 +8,9 @@ tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096")
 model = LongformerModel.from_pretrained("allenai/longformer-base-4096")
 text = "Replace me by any text you'd like."
 encoded_input = tokenizer(text, return_tensors="pt")
-exported = torch.export.export(model, (encoded_input["input_ids"], encoded_input["attention_mask"]))
+exported = torch.export.export(
+    model, (encoded_input["input_ids"], encoded_input["attention_mask"])
+)
 ir_model = torch_onnx.exported_program_to_ir(exported)
 proto = ir.to_proto(ir_model)
 onnx.save_model(proto, "longformer.onnx")

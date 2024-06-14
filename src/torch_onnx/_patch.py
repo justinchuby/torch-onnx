4db111a2d6aaec5ad9d8da74647799b121055748
Justin Chu <justinchuby@users.noreply.github.com>
2024-06-14 07:19:36 -0700

    Better errors

--- src/torch_onnx/_patch.py
+++ src/torch_onnx/_patch.py
@@ -37,23 +37,41 @@ def torch_onnx_export_adaptor(
         kwargs = args[-1]
         args = args[:-1]
     # TODO: Support dynamic shapes
-    program = torch.export.export(model, args, kwargs)
-    ir_model = torch_onnx.exported_program_to_ir(program)
+    try:
+        program = torch.export.export(model, args, kwargs)
+    except Exception as e:
+        raise RuntimeError(
+            "Failed to export the model with torch.export. "
+            "This is step 1/2 of exporting the model to ONNX. Please create an issue "
+            "in the PyTorch GitHub repository against the *torch.export* component and "
+            "attach the full error stack as well as reproduction scripts."
+        ) from e
 
-    if input_names:
-        _ir_passes.rename_inputs(ir_model, input_names)
-    if output_names:
-        _ir_passes.rename_outputs(ir_model, output_names)
+    try:
+        ir_model = torch_onnx.exported_program_to_ir(program)
 
-    proto = ir.serde.serialize_model(ir_model)
-    if proto.ByteSize() >= 1 << 31:
-        logger.warning(
-            "The serialized ONNX model is larger than 2GB. "
-            "Saving the weights in a separate file"
-        )
-        onnx.save_model(proto, f, save_as_external_data=True)
-    else:
-        onnx.save_model(proto, f)
+        if input_names:
+            _ir_passes.rename_inputs(ir_model, input_names)
+        if output_names:
+            _ir_passes.rename_outputs(ir_model, output_names)
+
+        proto = ir.serde.serialize_model(ir_model)
+        if proto.ByteSize() >= 1 << 31:
+            logger.warning(
+                "The serialized ONNX model is larger than 2GB. "
+                "Saving the weights in a separate file"
+            )
+            onnx.save_model(proto, f, save_as_external_data=True)
+        else:
+            onnx.save_model(proto, f)
+
+    except Exception as e:
+        raise RuntimeError(
+            "Failed to convert the exported program to an ONNX model. "
+            "This is step 2/2 of exporting the model to ONNX. Please create an issue "
+            "in the PyTorch GitHub repository against the *onnx* component and "
+            "attach the full error stack as well as reproduction scripts."
+        ) from e
 
     return ir_model
 

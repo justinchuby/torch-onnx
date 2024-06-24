b85ec552dcbb0615ecacb0d9ba25b4593da13832
Justin Chu <justinchu@microsoft.com>
2024-06-24 23:24:03 +0000

    Improve external data name

--- src/torch_onnx/_onnx_program.py
+++ src/torch_onnx/_onnx_program.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import pathlib
 from typing import IO
 
 import onnx
@@ -26,6 +27,7 @@ class ONNXProgram:
         destination: str | os.PathLike | IO[bytes],
         *,
         include_initializers: bool = True,
+        external_data: bool | None = None,
         **_,
     ):
         if not include_initializers:
@@ -33,12 +35,27 @@ class ONNXProgram:
                 "Developers: Please implement ir.Model copy() and remove initializers"
             )
         proto = ir.serde.serialize_model(self.model)
-        if proto.ByteSize() >= 1 << 31:
+        byte_size = proto.ByteSize()
+        model_too_large = (byte_size) >= 1 << 31
+        if external_data or model_too_large:
             # TODO: Create an IR pass to handle external tensors conversion
-            logger.warning(
-                "The serialized ONNX model is larger than 2GB. "
-                "Saving the weights in a separate file"
+            if model_too_large:
+                logger.warning(
+                    "The serialized ONNX model is larger than 2GB (%s). "
+                    "Saving the weights as external data in a separate file.",
+                    byte_size,
+                )
+            if not isinstance(destination, (str, os.PathLike)):
+                raise TypeError(
+                    "Saving the weights as external data is only supported when destination is a file path"
+                )
+            destination_path = pathlib.Path(destination)
+            data_path = destination_path.with_suffix(f"{destination_path.suffix}.data")
+            onnx.save_model(
+                proto,
+                destination,
+                save_as_external_data=True,
+                location=os.fspath(data_path),
             )
-            onnx.save_model(proto, destination, save_as_external_data=True)
         else:
             onnx.save_model(proto, destination)

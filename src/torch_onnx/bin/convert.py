5f9fb7718297a30eb4edb74efeb3d38c5a12ecae
Justin Chu <justinchu@microsoft.com>
2024-06-25 01:04:04 +0000

    bin

--- src/torch_onnx/bin/convert.py
+++ src/torch_onnx/bin/convert.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python  # noqa: EXE001
 """Convert a pt2 PyTorch model to ONNX format."""
 
 import argparse
-import logging
 
 
 def main(args):
@@ -10,32 +9,14 @@ def main(args):
 
     # Delay import to improve startup time.
     import torch
-    import torch_onnx
-    from onnxscript import ir
-    import onnx
+    from torch_onnx._core import export
 
     exported_program = torch.export.load(args.model_path)
-    onnx_model = torch_onnx.exported_program_to_ir(exported_program)
-    proto = ir.serde.serialize_model(onnx_model)
-    proto_bytesize = proto.ByteSize()
-    large_proto = proto_bytesize >= 1 << 31
-    if large_proto or args.external_data:
-        external_data_path = args.output_path + ".data"
-        if large_proto:
-            logging.warning(
-                "The serialized ONNX model is larger than 2GB. "
-                "Saving the weights in a separate file"
-            )
-        onnx.save_model(
-            proto,
-            args.output_path,
-            save_as_external_data=True,
-            location=external_data_path,
-        )
-    else:
-        onnx.save_model(proto, args.output_path)
-
-    # TODO: Display analysis info if the process fails.
+    onnx_program = export(
+        exported_program, (), {}, error_report=not args.no_error_report
+    )
+    del exported_program
+    onnx_program.save(args.output_path)
 
 
 if __name__ == "__main__":
@@ -43,9 +24,14 @@ if __name__ == "__main__":
         description="Convert a pt2 PyTorch model to ONNX format."
     )
     parser.add_argument("model_path", type=str, help="Path to the pt2 PyTorch model.")
-    parser.add_argument("--output_path", type=str, help="Path to save the ONNX model.")
+    parser.add_argument("output_path", type=str, help="Path to save the ONNX model.")
     parser.add_argument(
         "--external_data", type=bool, help="Save the weights in a separate file."
+    )
+    parser.add_argument(
+        "--no_error_report",
+        type=bool,
+        help="Do not produce an error report if the conversion fails.",
     )
     args = parser.parse_args()
     main(args)

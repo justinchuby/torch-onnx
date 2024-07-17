70cb8624ab6bace83a84be2afa854586f4268018
Justin Chu <justinchu@microsoft.com>
2024-07-17 02:19:41 +0000

    validate

--- src/torch_onnx/_validation.py
+++ src/torch_onnx/_validation.py
@@ -1,13 +1,26 @@
 from __future__ import annotations
 
-from torch_onnx import _onnx_program
+from typing import Any
+
 import torch
 from torch.utils import _pytree as pytree
 
+from torch_onnx import _onnx_program
 
-def verify_onnx_program(onnx_program: _onnx_program.ONNXProgram):
+
+def verify_onnx_program(
+    onnx_program: _onnx_program.ONNXProgram,
+    args: tuple[Any, ...] | None = None,
+    kwargs: dict[str, Any] | None = None,
+):
     exported_program = onnx_program.exported_program
-    args, kwargs = exported_program.example_inputs
+    if args is None and kwargs is None:
+        # User did not provide example inputs, use the default example inputs
+        args, kwargs = exported_program.example_inputs
+    if args is None:
+        args = ()
+    if kwargs is None:
+        kwargs = {}
     torch_module = exported_program.module()
     torch_outputs, _ = pytree.tree_flatten(torch_module(*args, **kwargs))
     onnx_outputs = onnx_program(*args, **kwargs)

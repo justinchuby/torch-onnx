88d3e38d6b839196ef551d60de1405175135ff15
Justin Chu <justinchu@microsoft.com>
2024-07-17 02:17:39 +0000

    verify

--- src/torch_onnx/_validation.py
+++ src/torch_onnx/_validation.py
@@ -7,9 +7,10 @@ from torch.utils import _pytree as pytree
 
 def verify_onnx_program(onnx_program: _onnx_program.ONNXProgram):
     exported_program = onnx_program.exported_program
-    inputs = exported_program.example_inputs
-    torch_outputs, _ = pytree.tree_flatten(exported_program.graph_module(*inputs))
-    onnx_outputs = onnx_program(*inputs)
+    args, kwargs = exported_program.example_inputs
+    torch_module = exported_program.module()
+    torch_outputs, _ = pytree.tree_flatten(torch_module(*args, **kwargs))
+    onnx_outputs = onnx_program(*args, **kwargs)
     for torch_output, onnx_output in zip(torch_outputs, onnx_outputs):
         # TODO: Use a data structure to store comparison results
-        torch.testing.assert_close(torch_output, onnx_output)
+        torch.testing.assert_close(torch_output, onnx_output, rtol=1e-3, atol=1e-4)

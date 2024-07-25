188a73a18717a63d1fbaaa96a3ce3e809011b876
Justin Chu <justinchuby@users.noreply.github.com>
2024-07-25 06:58:24 -0700

    tes

--- tests/jit_test.py
+++ tests/jit_test.py
@@ -1,5 +1,7 @@
+import unittest
 import torch
 import torch_onnx
+
 
 class Model(torch.nn.Module):
     def forward(self, arg0_1, arg1_1):
@@ -7,5 +9,11 @@ class Model(torch.nn.Module):
         return add
 
 
-traced = torch.jit.trace(Model(), (torch.rand(1), torch.rand(1)))
-print(torch_onnx.export(traced, (torch.rand(1), torch.rand(1))))
+class JitModelTest(unittest.TestCase):
+    def test_jit_model_can_be_exported(self):
+        traced = torch.jit.trace(Model(), (torch.rand(1), torch.rand(1)))
+        print(torch_onnx.export(traced, (torch.rand(1), torch.rand(1))))
+
+
+if __name__ == "__main__":
+    unittest.main()

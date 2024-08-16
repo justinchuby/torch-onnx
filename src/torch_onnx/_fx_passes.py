6fbc3f2ec7743753ba80332018298e895d985fe0
Justin Chu <justinchuby@users.noreply.github.com>
2024-08-16 13:14:05 -0700

    Use preserve_ops for decomposition (#151)
    Specify _preserve_ops to prevent upsample and linear from being
    decomposed.

--- src/torch_onnx/_fx_passes.py
+++ src/torch_onnx/_fx_passes.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import packaging.version
 import torch
 import torch.export
 import torch.fx
@@ -15,6 +16,16 @@ _ATEN_ASSERTION_TARGETS = frozenset(
 )
 
 
+def _torch_older_than(version: str) -> bool:
+    """Returns True if the torch version is older than the given version."""
+    import torch  # pylint: disable=import-outside-toplevel
+
+    return (
+        packaging.version.parse(torch.__version__).release
+        < packaging.version.parse(version).release
+    )
+
+
 def decompose_with_registry(
     exported_program: torch.export.ExportedProgram, registry: _registration.ONNXRegistry
 ) -> torch.export.ExportedProgram:
@@ -23,7 +34,37 @@ def decompose_with_registry(
     This function is needed so it shows clearly on the profiler results.
     """
     decomp_table = _decomp.create_onnx_friendly_decomposition_table(registry)
-    return exported_program.run_decompositions(decomp_table)
+    if _torch_older_than("2.5"):
+        return exported_program.run_decompositions(decomp_table)
+    else:
+        # The _preserve_ops argument is only available in torch>=2.5
+        onnx_registered_ops = set(_decomp.get_onnx_implemented_overloads(registry))
+        # Try to preserve some known CompositeImplicitAutograd ops
+        aten = torch.ops.aten
+        to_preserve = {
+            aten._upsample_bilinear2d_aa.default,
+            aten._upsample_nearest_exact1d.vec,
+            aten._upsample_nearest_exact2d.vec,
+            aten._upsample_nearest_exact3d.vec,
+            aten.linear.default,
+            aten.upsample_bilinear2d.default,
+            aten.upsample_bilinear2d.vec,
+            aten.upsample_linear1d.default,
+            aten.upsample_linear1d.vec,
+            aten.upsample_nearest1d.default,
+            aten.upsample_nearest1d.vec,
+            aten.upsample_nearest2d.default,
+            aten.upsample_nearest2d.vec,
+            aten.upsample_nearest3d.default,
+            aten.upsample_nearest3d.vec,
+            aten.upsample_trilinear3d.default,
+            aten.upsample_trilinear3d.vec,
+        }
+        # We can only preserve implemented ops
+        can_preserve = tuple(to_preserve.intersection(onnx_registered_ops))
+        return exported_program.run_decompositions(
+            decomp_table, _preserve_ops=can_preserve
+        )
 
 
 def insert_type_promotion_nodes(
---
+++ tests/models/upsample.py
@@ -0,0 +1,26 @@
+import torch
+import torch_onnx
+from monai.networks.nets import SegResNet
+
+torch_onnx.patch_torch(
+    report=True, profile=True, verify=True, dump_exported_program=False, fallback=False
+)
+
+
+def main():
+    model = SegResNet(
+        blocks_down=(1, 2, 2, 4),
+        blocks_up=(1, 1, 1),
+        init_filters=16,
+        in_channels=4,
+        out_channels=3,
+        dropout_prob=0.2,
+    ).eval()
+    data = torch.randn(1, 4, 224, 224, 128)
+
+    # Check that the upsample op is not decomposed
+    torch.onnx.export(model, (data,), "upsample.onnx")
+
+
+if __name__ == "__main__":
+    main()

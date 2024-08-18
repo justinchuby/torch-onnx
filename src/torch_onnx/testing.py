7145b487314bcc3cd1c3b4ce401a848b3db6e208
Justin Chu <justinchuby@users.noreply.github.com>
2024-08-18 10:17:02 -0700

    assert_onnx_program

--- src/torch_onnx/testing.py
+++ src/torch_onnx/testing.py
@@ -2,12 +2,15 @@
 
 from __future__ import annotations
 
-import torch
-from torch_onnx import _onnx_program, _verification
 from typing import Any
 
+import torch
+from torch.utils import _pytree
 
-def assert_model_accuracy(
+from torch_onnx import _onnx_program
+
+
+def assert_onnx_program(
     program: _onnx_program.ONNXProgram,
     *,
     atol: float | None = None,
@@ -15,18 +18,30 @@ def assert_model_accuracy(
     args: tuple[Any, ...] | None = None,
     kwargs: dict[str, Any] | None = None,
 ) -> None:
-    """Asserts that the ONNX model has the same output as the PyTorch model."""
+    """Asserts that the ONNX model produces the same output as the PyTorch ExportedProgram."""
 
-    verification_info = _verification.verify_onnx_program(
-        program, args=args, kwargs=kwargs
+    exported_program = program.exported_program
+    if args is None and kwargs is None:
+        # User did not provide example inputs, use the default example inputs
+        if exported_program.example_inputs is None:
+            raise ValueError(
+                "No example inputs provided and the exported_program does not contain example inputs. "
+                "Please provide arguments to verify the ONNX program."
+            )
+        args, kwargs = exported_program.example_inputs
+    if args is None:
+        args = ()
+    if kwargs is None:
+        kwargs = {}
+    torch_module = exported_program.module()
+    torch_outputs, _ = _pytree.tree_flatten(torch_module(*args, **kwargs))
+    onnx_outputs = program(*args, **kwargs)
+    # TODO(justinchuby): Include output names in the error message
+    torch.testing.assert_close(
+        tuple(torch_outputs),
+        tuple(onnx_outputs),
+        atol=atol,
+        rtol=rtol,
+        equal_nan=True,
+        check_device=False,
     )
-    for info in verification_info:
-        if atol is not None:
-            assert (
-                info.absolute_difference <= atol
-            ), f"Absolute difference is greater than {atol}"
-        if rtol is not None:
-            assert (
-                info.relative_difference <= rtol
-            ), f"Relative difference is greater than {rtol}"
-    return None

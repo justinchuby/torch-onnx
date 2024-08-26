babf4fc1133c1634bca9a3c9776095765ac18f41
Justin Chu <justinchu@microsoft.com>
2024-08-26 19:08:35 +0000

    Diff

---
+++ tests/models/senet154_diff.py
@@ -0,0 +1,26 @@
+import torch
+import torch_onnx
+import torch_onnx.tools
+import torch_onnx.tools.diff_model
+from monai.networks.nets import SENet154
+
+
+def main():
+    model = SENet154(spatial_dims=3, in_channels=2, num_classes=2).eval()
+    data = (torch.randn(2, 2, 64, 64, 64),)
+    # torch_onnx.export(model, data, verify=True, report=True)
+    ep = torch.export.export(model, data)
+    onnx_program = torch_onnx.export(model, data)
+    onnx_program.save("senet154.onnx", external_data=True)
+    results = torch_onnx.tools.diff_model.diff_exported_program(
+        "senet154.onnx",
+        ep,
+        ["output"],
+        data,
+        keep_original_outputs=True,
+    )
+    print(results)
+
+
+if __name__ == "__main__":
+    main()

89a9ce23bf7ff4687c29ebab128e66a3b1dd1d7d
Justin Chu <justinchu@microsoft.com>
2024-09-06 20:37:05 +0000

    error message

--- src/torch_onnx/_core.py
+++ src/torch_onnx/_core.py
@@ -120,7 +120,8 @@ class TorchTensor(ir.Tensor):
         if isinstance(self.raw, torch._subclasses.fake_tensor.FakeTensor):
             raise TypeError(
                 "Cannot take content out from a FakeTensor. Please replace the tensor "
-                "with a tensor backed by real data or save the model without initializers."
+                "with a tensor backed by real data using ONNXProgram.apply_weights() "
+                " or save the model without initializers by setting include_initializers=False."
             )
         tensor = self.raw.detach().cpu().contiguous()
         return bytes(

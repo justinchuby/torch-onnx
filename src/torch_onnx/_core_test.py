05f0629edd3b83960ae82504f0d3e84f847ea7c1
Justin Chu <justinchuby@users.noreply.github.com>
2024-09-18 09:34:50 -0700

    Sync with pytorch/pytorch#136162

--- src/torch_onnx/_core_test.py
+++ src/torch_onnx/_core_test.py
@@ -1,10 +1,13 @@
 # ruff: noqa: UP037
+# Owner(s): ["module: onnx"]
+"""Unit tests for the _core module."""
+
 from __future__ import annotations
 
-import unittest
-
+import numpy as np
 import torch
 from onnxscript import FLOAT
+from torch.testing._internal import common_utils
 
 import torch_onnx
 from torch_onnx import _core
@@ -27,7 +30,68 @@ u32 = torch.uint32
 u64 = torch.uint64
 
 
-class ExportedProgramToIrTest(unittest.TestCase):
+@common_utils.instantiate_parametrized_tests
+class TorchTensorTest(common_utils.TestCase):
+    @common_utils.parametrize(
+        "dtype, np_dtype",
+        [
+            (torch.bfloat16, np.uint16),
+            (torch.bool, np.bool_),
+            (torch.complex128, np.complex128),
+            (torch.complex64, np.complex64),
+            (torch.float16, np.float16),
+            (torch.float32, np.float32),
+            (torch.float64, np.float64),
+            (torch.float8_e4m3fn, np.uint8),
+            (torch.float8_e4m3fnuz, np.uint8),
+            (torch.float8_e5m2, np.uint8),
+            (torch.float8_e5m2fnuz, np.uint8),
+            (torch.int16, np.int16),
+            (torch.int32, np.int32),
+            (torch.int64, np.int64),
+            (torch.int8, np.int8),
+            (torch.uint16, np.uint16),
+            (torch.uint32, np.uint32),
+            (torch.uint64, np.uint64),
+            (torch.uint8, np.uint8),
+        ],
+    )
+    def test_numpy_returns_correct_dtype(self, dtype: torch.dtype, np_dtype):
+        tensor = _core.TorchTensor(torch.tensor([1], dtype=dtype))
+        self.assertEqual(tensor.numpy().dtype, np_dtype)
+        self.assertEqual(tensor.__array__().dtype, np_dtype)
+        self.assertEqual(np.array(tensor).dtype, np_dtype)
+
+    @common_utils.parametrize(
+        "dtype",
+        [
+            (torch.bfloat16),
+            (torch.bool),
+            (torch.complex128),
+            (torch.complex64),
+            (torch.float16),
+            (torch.float32),
+            (torch.float64),
+            (torch.float8_e4m3fn),
+            (torch.float8_e4m3fnuz),
+            (torch.float8_e5m2),
+            (torch.float8_e5m2fnuz),
+            (torch.int16),
+            (torch.int32),
+            (torch.int64),
+            (torch.int8),
+            (torch.uint16),
+            (torch.uint32),
+            (torch.uint64),
+            (torch.uint8),
+        ],
+    )
+    def test_tobytes(self, dtype: torch.dtype):
+        tensor = _core.TorchTensor(torch.tensor([1], dtype=dtype))
+        self.assertEqual(tensor.tobytes(), tensor.numpy().tobytes())
+
+
+class ExportedProgramToIrTest(common_utils.TestCase):
     def test_output_metadata_with_tuple_outputs(self):
         class GraphModule(torch.nn.Module):
             def forward(
@@ -85,4 +149,4 @@ class ExportedProgramToIrTest(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    common_utils.run_tests()
